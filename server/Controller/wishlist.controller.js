9918ef37cc72de75fa96493922adf5f2328383b4
kuldeep singh <kuldeeprathore74@gmail.com>
2021-12-03 02:15:17 +0530

    feat: wishlist delete functionality
    "DELETE /wishlist/61a24a11c40af6333cc4d507/61a3d2d76e2750afd608d782

--- server/Controller/wishlist.controller.js
+++ server/Controller/wishlist.controller.js
@@ -22,6 +22,7 @@ const getWishlistUsingId = async (req, res) => {
       wishlist,
     })
   } catch (error) {
+    console.log(error.stack)
     return res.status(500).json({
       success: false,
       message: "Internal server error",
@@ -29,4 +30,63 @@ const getWishlistUsingId = async (req, res) => {
     })
   }
 }
-module.exports = { getWishlistUsingId }
+
+const addToWishlistUsingId = async (req, res) => {
+  try {
+    const { userId, productId } = req.params
+    const wishlist = await Wishlist.findById(userId)
+    if (!wishlist) {
+      let wishlist = new Wishlist({
+        _id: userId,
+        wishlistItems: [{ product: productId }],
+      })
+      await wishlist.save()
+      return res.status(200).json({
+        success: true,
+        message: "first Use of wishlist",
+        wishlist,
+      })
+    }
+    wishlist.wishlistItems = wishlist.wishlistItems.concat({
+      product: productId,
+    })
+    await wishlist.save()
+    res.json({ success: true, message: "Product added to wishlist", wishlist })
+  } catch (error) {
+    console.log(error.stack)
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+      errorMessage: error.message,
+    })
+  }
+}
+const deleteFromWishlist = async (req, res) => {
+  try {
+    const { userId, productId } = req.params
+    const wishlist = await Wishlist.findById(userId)
+    const findIndex = wishlist.wishlistItems.findIndex(
+      (item) => item.product == productId
+    )
+    // wishlist.wishlistItems.splice(findIndex,1)
+    wishlist.wishlistItems[findIndex].remove()
+    await wishlist.save()
+    res.json({
+      success: true,
+      message: "Product deleted from wishlist",
+      wishlist,
+    })
+  } catch (error) {
+    console.log(error.stack)
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+      errorMessage: error.message,
+    })
+  }
+}
+module.exports = {
+  getWishlistUsingId,
+  addToWishlistUsingId,
+  deleteFromWishlist,
+}
--- server/Routes/wishlist.route.js
+++ server/Routes/wishlist.route.js
@@ -1,7 +1,15 @@
-const { getWishlistUsingId } = require("../Controller/wishlist.controller")
+const {
+  getWishlistUsingId,
+  addToWishlistUsingId,
+  deleteFromWishlist,
+} = require("../Controller/wishlist.controller")
 
 const router = require("express").Router()
 
 router.route("/:userId").get(getWishlistUsingId)
+router
+  .route("/:userId/:productId")
+  .post(addToWishlistUsingId)
+  .delete(deleteFromWishlist)
 
 module.exports = router
