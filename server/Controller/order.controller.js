1352e3d594b34b63a2a126d2b6fcd3450ec56e91
kuldeep singh <kuldeeprathore74@gmail.com>
2022-02-25 23:21:23 +0530

    catch block added

--- server/Controller/order.controller.js
+++ server/Controller/order.controller.js
@@ -27,18 +27,29 @@ const addToOrders = async (req, res) => {
 
 const getUserOrders = async (req, res) => {
   const { userId } = req
-  const orders = await Order.find({ userId }).populate("orderItems.product")
-  if (!orders) {
-    return res.status(404).json({ success: false, message: "no orders found" })
-  }
-  if (orders.length === 0) {
+  try {
+    const orders = await Order.find({ userId }).populate("orderItems.product")
+    if (!orders) {
+      return res
+        .status(404)
+        .json({ success: false, message: "no orders found" })
+    }
+    if (orders.length === 0) {
+      return res
+        .status(404)
+        .json({ success: false, message: "no orders found", orders: [] })
+    }
     return res
-      .status(404)
-      .json({ success: false, message: "no orders found", orders: [] })
+      .status(200)
+      .json({ success: true, message: "orders fectched successfully", orders })
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({
+      success: false,
+      message: "Error while fetching orders",
+      errorMessage: error.message,
+    })
   }
-  return res
-    .status(200)
-    .json({ success: true, message: "orders fectched successfully", orders })
 }
 
 module.exports = { addToOrders, getUserOrders }
